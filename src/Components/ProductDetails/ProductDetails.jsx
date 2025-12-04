4b216e6085d52b2176248592fbd8ae14abc8584b
SajibSaha207 <sajibsaha.207@gmail.com>
2025-12-05 00:55:45 +0600

    added bid midal

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -1,15 +1,47 @@
-import React, { useRef } from 'react';
+import React, { use, useRef } from 'react';
 import { useLoaderData } from 'react-router';
+import { AuthContext } from '../../Context/AuthContext';
 
 const ProductDetails = () => {
 
-    const product = useLoaderData();
+    const {_id: productId} = useLoaderData();
     const bidModalRef = useRef(null)
-    console.log(product)
+
+    const {user} = use(AuthContext);
+  
 
     const handleBidModalOpen = () =>{
         bidModalRef.current.showModal();
 
+    }
+
+    const handleBidSubmit = (e) =>{
+        e.preventDefault();
+
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const bid = e.target.bid.value;
+
+        console.log(productId, name, email, bid)
+
+        const newBid ={
+            product: productId,
+            buyer_name: name,
+            buyer_email: email,
+            bid_price: bid,
+            status: 'pending'
+        }
+        fetch('http://localhost:3000/bids',{
+            method: "POST",
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newBid)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log('after placing bid', data)
+        })
     }
     return (
         <div>
@@ -23,8 +55,35 @@ const ProductDetails = () => {
                      className='btn- btn-primary'>I want to buy</button>
 <dialog ref={bidModalRef} className="modal modal-bottom sm:modal-middle">
   <div className="modal-box">
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click the button below to close</p>
+    <h3 className="font-bold text-lg">Give the best offer!</h3>
+    <p className="py-4">Offer something seller can not resist</p>
+
+<form onSubmit={handleBidSubmit}>
+        <fieldset className="fieldset">
+
+          <label className="label">Name</label>
+          <input type="text" className="input" name='name' readOnly
+           defaultValue={user?.displayName || ""} />
+
+           {/* email */}
+          <label className="label">Email</label>
+          <input type="email" className="input" name='email' readOnly
+        defaultValue={user?.email || ""} />
+    
+{/* bid amount */}
+
+   <label className="label">Bid</label>
+          <input type="text" name='bid' className="input"  
+            placeholder='your bid amount' />
+
+
+          <button className="btn btn-neutral mt-4">Submit your Bid</button>
+        </fieldset>
+</form>
+
+
+
+
     <div className="modal-action">
       <form method="dialog">
         {/* if there is a button in form, it will close the modal */}
