8af27eba9f376ddd027013defba5965570cfee08
SajibSaha207 <sajibsaha.207@gmail.com>
2025-12-06 16:11:57 +0600

    added mybid page

--- src/Components/MyBids/MyBids.jsx
+++ src/Components/MyBids/MyBids.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
+import { AuthContext } from '../../Context/AuthContext';
 
 const MyBids = () => {
+
+    const {user} = use(AuthContext);
+    const [bids, setBids] = useState([])
+
+    useEffect(()=>{
+        if(user?.email){
+            fetch(`http://localhost:3000/bids?email=${user.email}`
+            )
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setBids(data)
+            })
+        }
+    }, [user?.email])
     return (
         <div>
-            
+            <h3>My Bids: {bids.length}</h3>
+            {/* table */}
+
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+         SL NO :
+        </th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Bid Price</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+    
+    {
+        bids.map((bid, index) =>   <tr key={bid._id}>
+        <th>
+       {index + 1}
+        </th>
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle h-12 w-12">
+                <img
+                  src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                  alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{bid.buyer_name}</div>
+             
+            </div>
+          </div>
+        </td>
+        <td>
+         {bid.buyer_email}
+          
+        </td>
+        <td>{bid.bid_price}</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>)
+    }
+  
+    
+    </tbody>
+ 
+  </table>
+</div>
         </div>
     );
 };
